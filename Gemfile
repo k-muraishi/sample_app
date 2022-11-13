3404a0e892e55cdf5b5a67d95a865c024804c329
“Kohei <muraishi634@gmail.com>
2022-11-13 19:53:54 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'sass-rails', '5.1.0'
 gem 'webpacker',  '4.0.7'
 gem 'turbolinks', '5.2.0'
 gem 'jbuilder',   '2.9.1'
+gem 'faker',  '2.20.0'
+gem 'will_paginate',           '3.3.1'
+gem 'bootstrap-will_paginate', '1.0.0'
 gem 'bootsnap',   '1.10.3', require: false
 
 group :development, :test do
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,19 +12,24 @@ module SessionsHelper
       cookies.permanent[:remember_token] = user.remember_token
     end
   
- # 記憶トークンcookieに対応するユーザーを返す
- def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.encrypted[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+    # 記憶トークンcookieに対応するユーザーを返す
+    def current_user
+        if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.encrypted[:user_id])
+        user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
+        end
     end
-  end
   
+    # 渡されたユーザーがカレントユーザーであればtrueを返す
+    def current_user?(user)
+        user && user == current_user
+    end
+      
     # ユーザーがログインしていればtrue、その他ならfalseを返す
     def logged_in?
       !current_user.nil?
@@ -43,10 +48,21 @@ module SessionsHelper
         cookies.delete(:remember_token)
       end
     
-      # 現在のユーザーをログアウトする
-      def log_out
+    # 現在のユーザーをログアウトする
+    def log_out
         forget(current_user)
         session.delete(:user_id)
         @current_user = nil
-      end
     end
+
+    # 記憶したURL（もしくはデフォルト値）にリダイレクト
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    # アクセスしようとしたURLを覚えておく
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
+end
