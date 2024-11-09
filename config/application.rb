15cb2da3d1588f80d2f934f7a91e6bec8e1a9f26
“Kohei <muraishi634@gmail.com>
2024-11-10 04:27:18 +0900

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,7 @@ module SampleApp
   class Application < Rails::Application
     config.load_defaults 6.0
     config.active_storage.variant_processor = :mini_magick
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
